0288bdc1d65f52280ea274b5db6b917312a644fa
Chris Bayles <cbayles81@gmail.com>
2012-02-23 22:29:22 -0600

    Finished problems 1 and 2

--- ProjectEuler/EulerTests.cs
+++ ProjectEuler/EulerTests.cs
@@ -7,12 +7,13 @@ namespace ProjectEuler
 	public class EulerTests
 	{
 		[Test]
-		public void Problem1_Given_10()
+		public void Problem1_Sum_of_multiples_of_3_or_5_below_10()
 		{
 			var problem = new Problem1();
 			var naturals = problem.FindNaturals(10);
 			//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 			//Find the sum of all the multiples of 3 or 5 below 1000.
+			Assert.That(naturals.Count, Is.EqualTo(4));
 			Assert.That(naturals, Contains.Item(3));
 			Assert.That(naturals, Contains.Item(5));
 			Assert.That(naturals, Contains.Item(6));
@@ -20,12 +21,13 @@ namespace ProjectEuler
 			Assert.That(problem.FindNaturalsSum(10), Is.EqualTo(23));
 		}
 		[Test]
-		public void Problem1_Given_1000()
+		public void Problem1_Sum_of_multiples_of_3_or_5_below_1000()
 		{
 			var problem = new Problem1();
 			var naturals = problem.FindNaturals(1000);
 			//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 			//Find the sum of all the multiples of 3 or 5 below 1000.
+			Assert.That(naturals.Count, Is.EqualTo(466));
 			Assert.That(naturals, Contains.Item(3));
 			Assert.That(naturals, Contains.Item(5));
 			Assert.That(naturals, Contains.Item(6));
@@ -38,7 +40,28 @@ namespace ProjectEuler
 			Assert.That(naturals, Contains.Item(500));
 			Assert.That(naturals, Contains.Item(600));
 			Assert.That(naturals, Contains.Item(900));
-			Assert.That(problem.FindNaturalsSum(1000), Is.EqualTo(2300000));
+			Assert.That(problem.FindNaturalsSum(1000), Is.EqualTo(233168));
+		}
+		
+		[Test]
+		public void Problem2_Sum_of_fibonacci_numbers_to_10()
+		{
+			var problem = new Problem2();
+			var numbers = problem.FibonaccisUpTo(10);
+			
+			Assert.That(numbers.Count, Is.EqualTo(5));
+			Assert.That(numbers, Contains.Item(1));
+			Assert.That(numbers, Contains.Item(2));
+			Assert.That(numbers, Contains.Item(3));
+			Assert.That(numbers, Contains.Item(5));
+			Assert.That(numbers, Contains.Item(8));
+			Assert.That(problem.FibonacciEvensSum(10), Is.EqualTo(10));
+		}
+		[Test]
+		public void Problem2_Sum_of_fibonacci_numbers_to_4_million()
+		{
+			var problem = new Problem2();
+			Assert.That(problem.FibonacciEvensSum(4000000), Is.EqualTo(4613732));
 		}
 	}
 }
--- ProjectEuler/Problem1.cs
+++ ProjectEuler/Problem1.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace ProjectEuler
 {
+	/// <summary>
+	/// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+	/// Find the sum of all the multiples of 3 or 5 below 1000.
+	/// </summary>
 	public class Problem1
 	{
 		public Problem1()
@@ -12,12 +17,18 @@ namespace ProjectEuler
 		
 		public List<int> FindNaturals(int seed)
 		{
-			throw new NotImplementedException();
+			var nattys = new List<int>();
+			for (int i = 1; i < seed; i++)
+			{
+				if (i%3==0||i%5==0)
+					nattys.Add(i);
+			}
+			return nattys;
 		}
 		
 		public int FindNaturalsSum(int seed)
 		{
-			throw new NotImplementedException();
+			return FindNaturals(seed).Sum();
 		}
 	}
 }
